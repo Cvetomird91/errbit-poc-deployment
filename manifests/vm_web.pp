62a30bc674c4a142b5f1b8b621a6d8c15d4ce997
Cvetomir Denchev <cvetomir_denchev@abv.bg>
2016-04-26 10:48:28 +0300

    added rvm module for web box

--- manifests/vm_web.pp
+++ manifests/vm_web.pp
@@ -8,15 +8,12 @@ package { $packages:
   provider => 'apt',
 }
 
-exec { 'get rvm gpg':
-  path => '/usr/local/bin:/usr/bin:/home/vagrant/.rvm/bin',
-  user => 'vagrant',
-  command => 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3',
-}
+class { '::rvm': }
 
-exec { 'install ruby 2.3.0':
-  path => '/usr/local/bin:/bin:/usr/bin:/home/vagrant/.rvm/bin/',
-  command => 'curl -sSL https://get.rvm.io | bash -s stable',
-  user => 'vagrant',
-  environment => ["HOME=/home/vagrant"]
+rvm::system_user { vagrant: }
+
+rvm_system_ruby {
+  'ruby-2.3.0':
+  ensure => 'present',
+  default_use => true,
 }
