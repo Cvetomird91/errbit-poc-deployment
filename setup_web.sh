8190b69d2820e0b0c8d49078cfe392292f38ed82
Cvetomir Denchev <cvetomir_denchev@abv.bg>
2016-05-03 13:50:35 +0300

    changed web setup shell script to add vscrepo module

--- setup_web.sh
+++ setup_web.sh
@@ -1,3 +1,4 @@
 sudo puppet module install -i /etc/puppet/modules/ puppetlabs-stdlib
 sudo puppet module install -i /etc/puppet/modules/ maestrodev/rvm
+sudo puppet module install -i /etc/puppet/modules/ puppetlabs-vcsrepo
 sudo service puppet restart
