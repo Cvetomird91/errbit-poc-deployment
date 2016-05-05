7f4d5481a5103712da67c50d023489222e41b10c
Cvetomir Denchev <cvetomir_denchev@abv.bg>
2016-05-05 17:48:38 +0300

    setup_web.sh updated

--- setup_web.sh
+++ setup_web.sh
@@ -1,4 +1,4 @@
 sudo puppet module install -i /etc/puppet/modules/ puppetlabs-stdlib
-sudo puppet module install -i /etc/puppet/modules/ maestrodev/rvm
 sudo puppet module install -i /etc/puppet/modules/ puppetlabs-vcsrepo
+sudo puppet module install -i /etc/puppet/modules/ puppetlabs-apt
 sudo service puppet restart
